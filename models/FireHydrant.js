09a4ea305c82e10700822cbf3fe8d25bb488012b
Nguyen Duy Thanh <nguyenduythanh25790@gmail.com>
2017-03-26 14:37:20 +0700

    cap nhat tru nuoc ( fire hydrant )

--- models/FireHydrant.js
+++ models/FireHydrant.js
@@ -3,10 +3,10 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 //
 module.exports = mongoose.model('FireHydrant', new Schema({
-    fireHydrantId: Number,
     name: String,
     address: String,
     desc: String,
     lat: Number,
     long: Number,
+    coordinates:[]
 }));
--- routes/FireHydrant.js
+++ routes/FireHydrant.js
@@ -6,6 +6,9 @@ var cf = require('../helpers/CF');
 var cors = require('cors'); //Cross domain request, using cors to make request from domain to another domain, cors is required for all backendservices
 var AuthMiddeWare = require('./AuthMiddeWare');
 var FireHydrant = require('../models/FireHydrant');
+var async = require('async');
+var ObjectID = require('mongodb').ObjectID;
+var _ = require('lodash');
 
 var router = express.Router();
 var jsonParser = bodyParser.json();
@@ -34,7 +37,6 @@ router.post('/InsertFireHydrant', jsonParser, function (req, res) {
     var body = req.body;
 
     var fireHydrant = new FireHydrant({
-        fireHydrantId: body.fireHydrantId,
         name: body.name,
         address: body.address,
         desc: body.desc,
@@ -46,6 +48,85 @@ router.post('/InsertFireHydrant', jsonParser, function (req, res) {
         if (err) {
             var responseObject = cf.buildResponse(responseCode.ERROR, 'message');
             res.status(500).send(responseObject);
+        } else {
+            var responseObject = cf.buildResponse(responseCode.SUCCESS, 'Success');
+            res.status(200).send(responseObject);
+        }
+    });
+});
+
+function getDistanceFromLatLonInMet(lat1, lon1, lat2, lon2) {
+    console.log(lat1);
+    console.log(lon1);
+    console.log(lat2);
+    console.log(lon2);
+    var R = 6371; // Radius of the earth in km
+    var dLat = deg2rad(lat2 - lat1);  // deg2rad below
+    var dLon = deg2rad(lon2 - lon1);
+    var a =
+        Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+        Math.sin(dLon / 2) * Math.sin(dLon / 2)
+        ;
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    var d = R * c * 1000; // Distance in m
+    return d;
+}
+
+function deg2rad(deg) {
+    return deg * (Math.PI / 180)
+}
+
+router.post('/NearByFireHydrant', jsonParser, function (req, res) {
+    async.waterfall([
+        function (callback) {
+            FireHydrant.find({}, function (err, docs) {
+                if (err) {
+                    callback(true, err);
+                } else {
+                    callback(null, docs);
+                }
+            });
+        },
+        function (docs, callback) {
+            //Get by id
+            var nearByPlaces = [];
+            _.forEach(docs, function (doc) {
+                if (getDistanceFromLatLonInMet(doc.lat, doc.long, req.body.lat, req.body.long) <= req.body.distance) {
+                    nearByPlaces.push(doc);
+                }
+            });
+            callback(null, nearByPlaces);
+        }
+    ], function (err, result) {
+        if (err) {
+            var responseObject = cf.buildResponse(responseCode.ERROR, err);
+            res.status(200).send(responseObject);
+        } else {
+            var responseObject = cf.buildResponse(responseCode.SUCCESS, 'Success');
+            responseObject.data = result;
+            res.status(200).send(responseObject);
+        }
+    });
+
+
+});
+
+router.post('/UpdateFireHydrant', jsonParser, function (req, res) {
+    var body = req.body;
+
+    FireHydrant.update({ _id: ObjectID(body.id) }, {
+        $set: {
+            name: body.name,
+            address: body.address,
+            desc: body.desc,
+            lat: body.lat,
+            long: body.long
+        }
+    }, function (err) {
+        if (err) {
+            var responseObject = cf.buildResponse(responseCode.ERROR, err);
+            res.status(200).send(responseObject);
         } else {
             var responseObject = cf.buildResponse(responseCode.SUCCESS, 'Success');
             res.status(200).send(responseObject);
